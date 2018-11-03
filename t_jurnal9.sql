2c21788e5f27be21b381f928bd15bccf0b3011ab
putriaulianti <42660888+putriaulianti@users.noreply.github.com>
2018-11-03 11:34:44 +0700

    Add files via upload

---
+++ t_jurnal9.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Nov 2018 pada 05.33
+-- Versi server: 10.1.34-MariaDB
+-- Versi PHP: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_jurnal9`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `t_jurnal9`
+--
+
+CREATE TABLE `t_jurnal9` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `usia` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `t_jurnal9`
+--
+
+INSERT INTO `t_jurnal9` (`id`, `nama`, `alamat`, `usia`) VALUES
+(2, 'Ahmad Tsauri Sajaya', 'Banjarbaru', 19),
+(3, 'Putri Aulianti Maulida Rumi', 'Banjarbaru', 19);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `t_jurnal9`
+--
+ALTER TABLE `t_jurnal9`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `t_jurnal9`
+--
+ALTER TABLE `t_jurnal9`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
